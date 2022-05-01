878a0b4ad7df0575a95d835ff7fb6a5c81eb977d
gongliang <2543034416@domain.com>
2022-05-01 11:10:01 +0800

    second commit

--- src/main/java/com/atguigu/test/helloTest.java
+++ src/main/java/com/atguigu/test/helloTest.java
@@ -3,5 +3,7 @@ package com.atguigu.test;
 public class helloTest {
     public static void main(String[] args) {
         System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
